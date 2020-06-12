f2e637d141f7d35e9b9b4562c89c3dac8691d0de
Zastin <jadbaxton@yandex.com>
2020-06-11 22:43:21 -0400

    Create __init__.py

---
+++ zzfunc/__init__.py
@@ -0,0 +1,21 @@
+from . import deband, mask, util
+
+y = util.get_y
+u = util.get_u
+v = util.get_v
+r = util.get_r
+g = util.get_g
+b = util.get_b
+
+w = util.width
+
+c = util.bicubic_c
+
+split = util.split
+join = util.join
+
+minfilter = util.MinFilter
+maxfilter = util.MaxFilter
+
+deband = deband.placebo
+f3kdb = deband.f3kdb
