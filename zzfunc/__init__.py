e336b7b6b5de9f9a1c8b5bafb93dc7e00435f319
Zastin <jadbaxton@yandex.com>
2020-06-13 04:51:10 -0400

    Update __init__.py

--- zzfunc/__init__.py
+++ zzfunc/__init__.py
@@ -3,6 +3,7 @@ from . import deband, mask, util
 y = util.get_y
 u = util.get_u
 v = util.get_v
+
 r = util.get_r
 g = util.get_g
 b = util.get_b
@@ -16,5 +17,14 @@ join = util.join
 minfilter = util.MinFilter
 maxfilter = util.MaxFilter
 
-deband = deband.placebo
 f3kdb = deband.f3kdb
+f3kpf = deband.highpass
+placebo = deband.placebo
+lfdeband = deband.low_res_f3kdb
+dbilateral = deband.Dither_gf3_bilateral_multistage
+
+dhhmask = mask.dhh
+colormask = mask.colors
+overlaymask = mask.overlays
+rangemask = mask.minmax
+lumamask = mask.luma
