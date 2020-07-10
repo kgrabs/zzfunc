931b30dbffb60c9f8cdd8e0b887e75d5ca5418f1
Zastin <jadbaxton@yandex.com>
2020-07-10 01:13:02 -0400

    Update __init__.py

--- zzfunc/__init__.py
+++ zzfunc/__init__.py
@@ -1,4 +1,4 @@
-from . import deband, mask, util
+from . import mask, mv, std, util
 
 y = util.get_y
 u = util.get_u
@@ -21,18 +21,17 @@ xpassfilter = util.xpassfilter
 pad = util.padding
 shiftplanes = util.shiftplanes
 shiftframes = util.shiftframes
-sfm = util.shiftframesmany
 
 mixp = util.mixed_depth
 
-f3kdb = deband.f3kdb
-f3kpf = deband.highpass
-placebo = deband.placebo
-lfdeband = deband.low_res_f3kdb
-dbilateral = deband.Dither_gf3_bilateral_multistage
+#f3kdb = deband.f3kdb
+#f3kpf = deband.highpass
+#placebo = deband.placebo
+#lfdeband = deband.low_res_f3kdb
+#dbilateral = deband.Dither_gf3_bilateral_multistage
 
-dhhmask = mask.dhh
-colormask = mask.colors
-overlaymask = mask.overlays
-rangemask = mask.minmax
-lumamask = mask.luma
+#dhhmask = mask.dhh
+#colormask = mask.colors
+#overlaymask = mask.overlays
+#rangemask = mask.minmax
+#lumamask = mask.luma
